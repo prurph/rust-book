041104e711970e8ba9d644b993ff63fd2212bf39
Prescott Murphy <prurph@gmail.com>
2023-12-31 16:30:52 -0500

    Complete structs project

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,23 +4,59 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let scale = 2;
-    let rect = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 
-    dbg!(&rect);
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
-    println!("The rectangle is: {:#?}", rect);
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect)
-    );
+    fn set_width(&mut self, width: u32) {
+        self.width = width;
+    }
 }
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(15 * scale),
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    dbg!(&rect1);
+
+    println!("rect1 is: {:#?}", rect1);
+
+    println!("The area of the rect1 is {} square pixels.", &rect1.area());
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let mut square1 = Rectangle::square(10);
+
+    println!("The area of square1 is {} square pixels.", &square1.area());
+
+    square1.set_width(100);
+
+    println!(
+        "Now the area of square1 is {} square pixels.",
+        &square1.area()
+    );
 }
