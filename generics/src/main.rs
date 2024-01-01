acb8c046411f11d4da58220610b41370c44f0050
Prescott Murphy <prurph@gmail.com>
2024-01-01 18:28:32 -0500

    Implement generic function for largest list value

---
+++ generics/src/main.rs
@@ -0,0 +1,21 @@
+use std::cmp::PartialOrd;
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    println!("The largest number is {}", largest(&number_list));
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    println!("The largest char is {}", largest(&char_list));
+}
