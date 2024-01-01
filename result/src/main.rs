c7f34a3a56fe4601f0e255eb40cfd8f4b065e507
Prescott Murphy <prurph@gmail.com>
2024-01-01 18:02:32 -0500

    Demonstrate ? operator and main returning Result

--- result/src/main.rs
+++ result/src/main.rs
@@ -1,6 +1,16 @@
-use std::fs::File;
+use std::error::Error;
+use std::fs;
 
-fn main() {
-    let _greeting_file_result =
-        File::open("hello.txt").expect("hello.txt should be included in this project");
+fn main() -> Result<(), Box<dyn Error>> {
+    // Failure to read immediately exits with the error instead
+    // and a non-zero status
+    let file_contents = fs::read_to_string("hello.txt")?;
+    println!("{:?}", last_char_of_first_line(&file_contents));
+
+    // Otherwise exit with 0 status
+    Ok(())
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
