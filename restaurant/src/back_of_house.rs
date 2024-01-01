114313d69bb9b3d9f44b42e989b79675369770ef
Prescott Murphy <prurph@gmail.com>
2024-01-01 12:41:19 -0500

    Complete restaurant project by factoring into submodules

---
+++ restaurant/src/back_of_house.rs
@@ -0,0 +1,7 @@
+pub mod meals;
+
+// All variants of a public enum are public
+pub enum Appetizer {
+    Soup,
+    Salad,
+}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,57 +1,15 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod back_of_house;
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_pament() {}
-    }
-}
-
-mod back_of_house {
-    // Struct fields are private by default
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    // All variants of a public enum are public
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-}
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // Summer breakfast with Rye toast
-    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // If we put `pub use meals::*;` in *back_of_house.rs*, this could be:
+    // `back_of_house::Breakfast` directly w/o `meals::`
+    let mut meal = back_of_house::meals::Breakfast::summer("Rye");
     // Actually I changed my mind
     meal.toast = String::from("White");
     println!("I'd like {} toast please", meal.toast);
@@ -63,5 +21,3 @@ pub fn eat_at_restaurant() {
     let order1 = back_of_house::Appetizer::Soup;
     let order1 = back_of_house::Appetizer::Salad;
 }
-
-fn deliver_order() {}
