1f5af472c82771437c27ea146ef4ece5e3422391
Prescott Murphy <prurph@gmail.com>
2024-01-12 18:10:55 -0500

    Add blog2 project contrasting encoding state into type system

---
+++ blog2/src/main.rs
@@ -0,0 +1,13 @@
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate blueberries for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate blueberries for lunch today", post.content());
+}
